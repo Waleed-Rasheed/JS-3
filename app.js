602777d77df2a58346d178aa3532705af84a59e4
Waleed-Rasheed <usamapagal75@gmail.com>
2024-02-23 07:15:50 +0500

    Work Done

---
+++ app.js
@@ -0,0 +1,65 @@
+//Get user to put a number using prompt () check if the number is a multiply of a 6 or not?
+
+let num = +prompt("Enter Your Numer")
+
+if (num%6 === 0){
+    document.write("True")
+}
+else{
+    document.write("False")
+}
+
+//==============================================================================================
+
+
+
+// Q2: Write a code which can give grade to student according to their scores?
+
+// let scores = +prompt("Enter your percentage" );
+// let grade;
+
+// if(scores >=90 && scores <=100 ){
+//     document.write("A+");
+// }else if(scores >=70 && scores <=89 ){
+//     document.write("A");
+// }else if(scores >=50 && scores <=69 ){
+//     document.write("B");
+// }else if(scores >=40 && scores <=49 ){
+//     document.write("C") ;
+// }else if(scores >=0 && scores <=39 ){
+//     document.write("Fail");
+// }else {
+//     document.write("Invalid");
+// }
+
+
+//===============================================================================================
+
+
+// Q3: Create marksheet using if else AND else if condition?
+
+// var Name = prompt("Name")
+// var English = +prompt("English")
+// var Urdu = +prompt("Urdu")
+// var Mathematics = +prompt("Mathematics ")
+// var Science = +prompt("Science")
+// var PakistanStudies = +prompt("PakistanStudies") 
+// var Total = English + Urdu + Mathematics + Science + PakistanStudies
+// var percentage = (Total / 500) * 100;
+
+
+// if(percentage >=90 && percentage <=100 ){
+//     document.write(Name + " Your grade is A+ " + percentage +"%");
+// }else if(percentage >=70 && percentage <=89 ){
+//     document.write(Name + " Your grade is A " + percentage +"%");
+// }else if(percentage >=50 && percentage <=69 ){
+//     document.write(Name + " Your grade is B " + percentage +"%");
+// }else if(percentage >=40 && percentage <=49 ){
+//     document.write(Name + " Your grade is C " + percentage +"%");
+// }else if(percentage >=0 && percentage <=39 ){
+//     document.write(Name + " Your are Fail " + percentage +"%");
+// }else {
+//     document.write("Invalid");
+// }
+
+//================================================================================================
